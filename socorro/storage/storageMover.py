9e87e0b67b8442698da0f01c2ae414b77a64f6b6
K Lars Lohn <twobraids@gmail.com>
2012-02-22 16:40:39 -0800

    mid dev checkin

--- socorro/storage/storageMover.py
+++ socorro/storage/storageMover.py
@@ -1,92 +1,97 @@
+#! /usr/bin/env python
+"""move new crashes from one storage system to another"""
+
+# This app can be invoked like this:
+#     .../socorro/app/example_app.py --help
+# set your path to make that simpler
+# set both socorro and configman in your PYTHONPATH
+
+import datetime
+
+from socorro.app.generic_app import App, main
+
+from configman import Namespace
+
 try:
-  import json
+    import json
 except ImportError:
-  import simplejson as json
+    import simplejson as json
 
 import signal
 
 import socorro.lib.JsonDumpStorage as jds
 import socorro.storage.crashstorage as cstore
 import socorro.lib.util as sutil
-import socorro.lib.iteratorWorkerFramework as iwf
+from socorro.lib.threaded_task_manager import ThreadedTaskManager
 
 
-#-------------------------------------------------------------------------------
-def move (conf,
-          sourceCrashStorageClass=cstore.CrashStorageSystemForLocalFS,
-          destCrashStorageClass=cstore.CrashStorageSystemForHBase):
-  logger = conf.logger
-  crashStoragePoolForSource = cstore.CrashStoragePool(conf, sourceCrashStorageClass)
-  crashStoragePoolForDest = cstore.CrashStoragePool(conf, destCrashStorageClass)
-  signal.signal(signal.SIGTERM, iwf.respondToSIGTERM)
-  signal.signal(signal.SIGHUP, iwf.respondToSIGTERM)
+#==============================================================================
+class StorageMoverApp(App):
+    app_name = 'storage_mover'
+    app_version = '2.0'
+    app_description = __doc__
 
-  #-----------------------------------------------------------------------------
-  def theIterator():
-    """This infinite iterator will walk through the file system storage,
-    yielding the ooids of every new entry in the filelsystem.  If there
-    are no new entries, it yields None"""
-    sourceStorage = crashStoragePoolForSource.crashStorage() # thread local
-    try:
-      while True:
-        i = 0
-        for i, ooid in enumerate(sourceStorage.newUuids()):
-          yield ooid
-        if i == 0:
-          yield None
-    except KeyboardInterrupt:
-      pass
-  #-----------------------------------------------------------------------------
+    #--------------------------------------------------------------------------
+    # in this section, define any configuration requirements
+    required_config = Namespace()
+    required_config.add_option('task_manager_class',
+                               default=ThreadedTaskManager,
+                               doc='the class that runs tasks')
+    required_config.source = Namespace()
+    required_config.source.add_option('storage_class',
+                                default=cstore.CrashStorageSystemForLocalFS,
+                                doc='the class for the source of new crashes')
+    required_config.destination = Namespace()
+    required_config.destination.add_option('storage_class',
+                                default=cstore.CrashStorageSystemForHBase,
+                                doc='the class for the destination of new '
+                                    'crashes')
 
-  #-----------------------------------------------------------------------------
-  def doSubmission(ooidTuple):
-    logger.debug('received: %s', str(ooidTuple))
-    try:
-      sourceStorage = crashStoragePoolForSource.crashStorage()
-      destStorage = crashStoragePoolForDest.crashStorage()
-      ooid = ooidTuple[0]
-      try:
-        jsonContents = sourceStorage.get_meta(ooid)
-      except ValueError:
-        logger.warning('the json for %s is degenerate and cannot be loaded'  \
-                       ' - saving empty json', ooid)
-        jsonContents = {}
-      dumpContents = sourceStorage.get_raw_dump(ooid)
-      logger.debug('pushing %s to dest', ooid)
-      result = destStorage.save_raw(ooid, jsonContents, dumpContents)
-      if result == cstore.CrashStorageSystem.ERROR:
-        return iwf.FAILURE
-      elif result == cstore.CrashStorageSystem.RETRY:
-        return iwf.RETRY
-      sourceStorage.quickDelete(ooid)
-      return iwf.OK
-    except Exception, x:
-      sutil.reportExceptionAndContinue(logger)
-      return iwf.FAILURE
-  #-----------------------------------------------------------------------------
+    #--------------------------------------------------------------------------
+    def __init__(self, config):
+        super(StorageMoverApp,self).__init__(config)
+        self.source = config.source.storage_class(config)
+        self.destination = config.destination.storage_class(config)
 
-  submissionMill = iwf.IteratorWorkerFramework(conf,
-                                               jobSourceIterator=theIterator,
-                                               taskFunc=doSubmission,
-                                               name='submissionMill')
+    #--------------------------------------------------------------------------
+    def the_iterator(self):
+        """This infinite iterator will walk through the file system storage,
+        yielding the ooids of every new entry in the filelsystem.  If there
+        are no new entries, it yields None"""
+        try:
+            while True:
+                i = 0
+                for i, ooid in enumerate(self.source.newUuids()):
+                    yield (ooid,)
+                if i == 0:
+                    yield None
+        except KeyboardInterrupt:
+            self.logger.info()
 
-  try:
-    submissionMill.start()
-    submissionMill.waitForCompletion() # though, it only ends if someone hits
-                                       # ^C or sends SIGHUP or SIGTERM - any of
-                                       # which will get translated into a
-                                       # KeyboardInterrupt exception
-  except KeyboardInterrupt:
-    while True:
-      try:
-        submissionMill.stop()
-        break
-      except KeyboardInterrupt:
-        logger.warning('We heard you the first time.  There is no need for '
-                       'further keyboard or signal interrupts.  We are '
-                       'waiting for the worker threads to stop.  If this app '
-                       'does not halt soon, you may have to send SIGKILL '
-                       '(kill -9)')
+    #--------------------------------------------------------------------------
+    def do_submission(self, ooid):
+        try:
+            try:
+                jsonContents = self.source.get_meta(ooid)
+            except ValueError:
+                logger.warning('the json for %s is degenerate and cannot '
+                               'be loaded - saving empty json', ooid)
+                jsonContents = {}
+            dumpContents = self.source.get_raw_dump(ooid)
+            logger.debug('pushing %s to dest', ooid)
+            self.destination.save_raw(ooid,
+                                      jsonContents,
+                                      dumpContents)
+            sourceStorage.quickDelete(ooid)
+        except Exception:
+            sutil.reportExceptionAndContinue(self.config.logger)
 
+    #--------------------------------------------------------------------------
+    def main(self):
+        task_manager = self.config.task_manager_class(self.config)
+        task_manager.blocking_start()
+
+if __name__ == '__main__':
+    main(ExampleApp)
 
 
