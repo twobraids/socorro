4f70cd81bddfbad3a9453bcea906c864c8e93cb2
K Lars Lohn <twobraids@gmail.com>
2013-01-30 15:38:03 -0800

    added fake products service

---
+++ socorro/external/filesystem/products.py
@@ -0,0 +1,11 @@
+class Products(object):
+
+    def get(self, **kwargs):
+        return {
+            "products": ["Fennicky"],
+            "hits": { "Fennicky", "1.0"},
+            "total": 1
+        }
+
+
+
