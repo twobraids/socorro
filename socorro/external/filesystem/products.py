76a994a668f975b601e02453673bf5fbe0a36727
K Lars Lohn <twobraids@gmail.com>
2013-01-30 18:50:30 -0800

    true form of product return

--- socorro/external/filesystem/products.py
+++ socorro/external/filesystem/products.py
@@ -4,9 +4,32 @@ class Products(FileSystemServiceBase):
 
     def get(self, **kwargs):
         return {
-            "products": ["Fennicky"],
-            "hits": { "Fennicky": ["1.0",]},
-            "total": 1
+            "products": [
+                "Fennecky",
+                "WaterWolf",
+                "Caminimal"
+            ],
+            "hits": {
+                "Fennecky": [
+                    {
+                        "product": "Fennecky",
+                        "version": "42",
+                        "start_date": "2001-01-01",
+                        "end_date": "2099-01-01",
+                        "throttle": 10.0,
+                        "featured": false,
+                        "release": "Nightly",
+                        "has_builds": true
+                    },
+                ],
+                "Thunderbird": [
+                    {}
+                ],
+                "Caminimal": [
+                    {}
+                ]
+            },
+            "total": 6
         }
 
 
