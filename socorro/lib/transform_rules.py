dfd5d515a3ca3bd4783da4a8dfe2643b92cbf1ba
K Lars Lohn <lars@mozilla.com>
2013-07-03 07:27:15 -0700

    classes for transform rules & skunk classifiers as classes

--- socorro/lib/transform_rules.py
+++ socorro/lib/transform_rules.py
@@ -5,6 +5,7 @@
 import re
 import configman
 import collections
+import inspect
 
 #------------------------------------------------------------------------------
 # support methods
@@ -56,6 +57,9 @@ class TransformRule(object):
             self.predicate = configman.converters.class_converter(predicate)
         except TypeError:
             self.predicate = predicate
+        if inspect.isclass(self.predicate):
+            self._predicitate_implementation = self.predicate()
+            self.predicate = self._predicitate_implementation.predicate
 
         try:
             if predicate_args in ('', None):
@@ -73,6 +77,9 @@ class TransformRule(object):
             self.action = configman.class_converter(action)
         except TypeError:
             self.action = action
+        if inspect.isclass(self.action):
+            self._action_implementation = self.action()
+            self.action = self._action_implementation.action
         try:
             if action_args in ('', None):
                 self.action_args = ()
--- socorro/processor/skunk_classifiers.py
+++ socorro/processor/skunk_classifiers.py
@@ -1,178 +1,191 @@
-from socorro.lib.util import DotDict
-"""
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-"""
+from socorro.lib.transform_rules import TransformRule
 
-
-#==============================================================================
-# Predicates
-
-#------------------------------------------------------------------------------
-def always_true_predicate(raw_crash, processed_crash, processor):
-    return True
-
-#------------------------------------------------------------------------------
-def classification_already_set_predicate(
-    raw_crash,
-    processed_crash,
-    processor
-):
-    try:
-        if 'classification' in processed_crash.classifications.skunk_works:
-            return False
-        return True
-    except KeyError:
-        return True
 
 
 #==============================================================================
-# Utilities
+class SkunkClassificationRule(TransformRule):
 
-#------------------------------------------------------------------------------
-def _add_classification(
-    processed_crash,
-    category,
-    classification,
-    classification_data
-):
-    if 'classifications' not in processed_crash:
-        processed_crash.classifications = DotDict()
-    processed_crash.classifications[category] = DotDict({
-        'classification': classification,
-        'classification_data': classification_data
-    })
+    #--------------------------------------------------------------------------
+    def predicate(self, raw_crash,  processed_crash, processor):
+        try:
+            if 'classification' in processed_crash.classifications.skunk_works:
+                return False
+            return True
+        except KeyError:
+            return True
 
-
-#------------------------------------------------------------------------------
-def _get_stack(processed_crash, dump_name='plugin'):
-    try:
-        a_json_dump = processed_crash[dump_name].json_dump
-    except KeyError:
-        # no plugin or plugin json dump
-        return False
-    try:
-        stack = a_json_dump.threads[a_json_dump.crash_info.crashing_thread]
-    except KeyError:
-        # no threads or no crash_info or no crashing_thread
-        return False
-    except IndexError:
-        # no stack for crashing_thread
-        return False
-    except ValueError:
-        # crashing_thread is not an integer
-        return False
-    return stack
-
-#------------------------------------------------------------------------------
-def _stack_contains(
-    stack,
-    signature,
-    a_signature_tool,
-    cache_normalizations=True
-):
-    found = False
-    for a_frame
-    reduce(
-        lambda x, y: x or y.startswith(signature),
-        signature,
-        False
-    )
-
-#==============================================================================
-# Rules
-
-#------------------------------------------------------------------------------
-def update_window_attributes_classifier(raw_crash, processed_crash, processor):
-    stack = _get_stack(processed_crash)
-    if stack is False:
+    #--------------------------------------------------------------------------
+    def action(self, raw_crash,  processed_crash, processor):
         return False
 
-    # normalize the stack of the crashing thread and find the 3 target frames
-    target_signatures = [
-        "F_1152915508___________________________________",
-        "mozilla::plugins::PluginInstanceChild::UpdateWindowAttributes(bool)",
-        "mozilla::ipc::RPCChannel::Call(IPC::Message*, IPC::Message*)"
-    ]
+    #--------------------------------------------------------------------------
+    def version(self):
+        return '0.0'
 
-    current_target_signature = target_signatures.pop()
-    for i, a_frame in enumerate(stack):
-        if (processor.c_signature_tool.normalize_signature(**a_frame) ==
-            current_target_signature):
-            current_target_signature = target_signatures.pop()
-            if not target_signatures:
-                break
-    if target_signatures:
-        return False
-
-    try:
-        classification_data = processor.c_signature_tool.normalize_signature(
-            stack[i + 1]
-        )
-    except IndexError:
-        classification_data = None
-
-    _add_classification(
+    #--------------------------------------------------------------------------
+    def _add_classification_to_processed_crash(
+        self,
         processed_crash,
-        'skunk_works',
-        'adbe-3355131',
+        category,
+        classification,
         classification_data
-    )
+    ):
+        if 'classifications' not in processed_crash:
+            processed_crash.classifications = DotDict()
+        processed_crash.classifications[category] = DotDict({
+            'classification': classification,
+            'classification_data': classification_data,
+            'classification_version': self.version()
+        })
 
-    return True
+    #--------------------------------------------------------------------------
+    @staticmethod
+    def _get_stack(processed_crash, dump_name='plugin'):
+        try:
+            a_json_dump = processed_crash[dump_name].json_dump
+        except KeyError:
+            # no plugin or plugin json dump
+            return False
+        try:
+            stack = a_json_dump.threads[a_json_dump.crash_info.crashing_thread]
+        except KeyError:
+            # no threads or no crash_info or no crashing_thread
+            return False
+        except IndexError:
+            # no stack for crashing_thread
+            return False
+        except ValueError:
+            # crashing_thread is not an integer
+            return False
+        return stack
 
+    #--------------------------------------------------------------------------
+    @staticmethod
+    def _stack_contains(
+        stack,
+        signature,
+        a_signature_tool,
+        cache_normalizations=True
+    ):
+        for a_frame in stack:
+            try:
+                normalized_frame = a_frame['normalized']
+            except KeyError:
+                normalized_frame = a_signature_tool.normalize(**a_frame)
+                if cache_normalizations:
+                    a_frame['normalized'] = normalized_frame
+            if normalized_frame.startswith(signature):
+                return True
+        return False
 
-#------------------------------------------------------------------------------
-def classify_set_window_pos(raw_crash, processed_crash, processor):
-    found = _do_set_window_pos_classification(
-        processed_crash,
-        'plugin',
-        ('F_320940052', 'F_1378698112', 'F_468782153')
-    )
-    if not found:
-        found = _do_set_window_pos_classification(
-            processed_crash,
-            'flash2',
-            ('F455544145',)
-        )
-    return found
+#==============================================================================
+class UpdateWindowAttributes(SkunkClassificationRule):
+    #--------------------------------------------------------------------------
+    def version(self):
+        return '0.1'
 
-#------------------------------------------------------------------------------
-def _do_set_window_pos_classification(
-    processed_crash,
-    dump_name,
-    secondary_sentinels
-):
-    truncated_stack = _get_stack(processed_crash, dump_name)[:5]
-    stack_contains_sentinel = reduce(
-        lambda x, y: x or y.startswith('NtUserSetWindowPos'),
-        truncated_stack,
-        False
-    )
-    if stack_contains_sentinel:
-        for a_second_sentinel in secondary_sentinels:
-            stack_contains_secondary = reduce(
-                lambda x, y: x or y.startswith(a_second_sentinel),
-                truncated_stack,
-                False
-            )
-            if stack_contains_secondary:
-                    _add_classification(
-                        processed_crash,
-                        'skunk_works',
-                        'NtUserSetWindowPos | %s' % a_second_sentinel,
-                        None
-                    )
-                    return True
+    #--------------------------------------------------------------------------
+    def action(raw_crash,  processed_crash, processor):
+        stack = _get_stack(processed_crash)
+        if stack is False:
+            return False
+
+        # normalize the stack of the crashing thread
+        # then look for these 3 target frames
+        target_signatures = [
+            "F_1152915508___________________________________",
+            "mozilla::plugins::PluginInstanceChild::UpdateWindowAttributes"
+                "(bool)",
+            "mozilla::ipc::RPCChannel::Call(IPC::Message*, IPC::Message*)"
+        ]
+
+        current_target_signature = target_signatures.pop()
+        for i, a_frame in enumerate(stack):
+            if (processor.c_signature_tool.normalize_signature(**a_frame) ==
+                current_target_signature):
+                current_target_signature = target_signatures.pop()
+                if not target_signatures:
+                    break
+        if target_signatures:
+            return False
+
+        try:
+            classification_data = \
+                processor.c_signature_tool.normalize_signature(stack[i + 1])
+        except IndexError:
+            classification_data = None
+
         _add_classification(
             processed_crash,
             'skunk_works',
-            'NtUserSetWindowPos | other',
-            None
+            'adbe-3355131',
+            classification_data
         )
+
         return True
-    return False
 
 
+#==============================================================================
+class SetWindowPos(SkunkClassificationRule):
+    #--------------------------------------------------------------------------
+    def version(self):
+        return '0.1'
 
+    #--------------------------------------------------------------------------
+    def action(raw_crash,  processed_crash, processor):
+        found = self._do_set_window_pos_classification(
+            processed_crash,
+            processor.c_signature_tool,
+            'plugin',
+            ('F_320940052', 'F_1378698112', 'F_468782153')
+        )
+        if not found:
+            found = self._do_set_window_pos_classification(
+                processed_crash,
+                processor.c_signature_tool,
+                'flash2',
+                ('F455544145',)
+            )
+        return found
 
-
+    #--------------------------------------------------------------------------
+    def _do_set_window_pos_classification(
+        self,
+        processed_crash,
+        signature_normalization_tool,
+        dump_name,
+        secondary_sentinels
+    ):
+        truncated_stack = self._get_stack(processed_crash, dump_name)[:5]
+        stack_contains_sentinel = self._stack_contains(
+            truncated_stack,
+            'NtUserSetWindowPos',
+            signature_normalization_tool,
+        )
+        if stack_contains_sentinel:
+            for a_second_sentinel in secondary_sentinels:
+                stack_contains_secondary = self._stack_contains(
+                    truncated_stack,
+                    a_second_sentinel,
+                    signature_normalization_tool,
+                )
+                if stack_contains_secondary:
+                        self._add_classification(
+                            processed_crash,
+                            'skunk_works',
+                            'NtUserSetWindowPos | %s' % a_second_sentinel,
+                            None
+                        )
+                        return True
+            self._add_classification(
+                processed_crash,
+                'skunk_works',
+                'NtUserSetWindowPos | other',
+                None
+            )
+            return True
+        return False
