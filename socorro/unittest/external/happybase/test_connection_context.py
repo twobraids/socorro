f75b2b9058b1441861e9c42c04b93888f18340ab
K Lars Lohn <twobraids@gmail.com>
2014-03-13 10:20:15 -0700

    another cut

--- socorro/unittest/external/happybase/test_connection_context.py
+++ socorro/unittest/external/happybase/test_connection_context.py
@@ -5,7 +5,14 @@
 import unittest
 import mock
 
+import happybase
+
 from socorro.external.happybase import connection_context
+from socorro.external.happybase.connection_context import (
+    HappyBaseConnectionContext,
+    HappyBasePooledConnectionContext,
+)
+
 from socorro.lib.util import SilentFakeLogger, DotDict
 from socorro.database.transaction_executor import TransactionExecutor
 from configman import Namespace
@@ -14,7 +21,7 @@ from socket import timeout, error
 
 
 class FakeHB_Connection(object):
-    def __init__(self, config):
+    def __init__(self, config, *args, **kwargs):
         self.close_counter = 0
         self.commit_counter = 0
         self.rollback_counter = 0
@@ -28,6 +35,9 @@ class FakeHB_Connection(object):
     def rollback(self):
         self.rollback_counter += 1
 
+class FakeHBPoolConnection(object):
+    def connection(self):
+        yield self
 
 class TestConnectionContext(unittest.TestCase):
     def test_basic_hbase_usage(self):
@@ -39,9 +49,9 @@ class TestConnectionContext(unittest.TestCase):
           'logger': SilentFakeLogger(),
         })
         a_fake_hbase_connection = FakeHB_Connection(local_config)
-        with mock.patch.object(connection_context, 'HBaseConnection',
+        with mock.patch.object(happybase, 'Connection',
                                mock.Mock(return_value=a_fake_hbase_connection)):
-            hb_context = connection_context.HBaseConnectionContext(
+            hb_context = connection_context.HappyBaseConnectionContext(
                 local_config
             )
             # open a connection
@@ -82,9 +92,9 @@ class TestConnectionContext(unittest.TestCase):
           'logger': SilentFakeLogger(),
         })
         a_fake_hbase_connection = FakeHB_Connection(local_config)
-        with mock.patch.object(connection_context, 'HBaseConnection',
+        with mock.patch.object(HappyBaseConnectionContext, 'connection',
                                mock.Mock(return_value=a_fake_hbase_connection)):
-            hb_context = connection_context.HBaseConnectionContext(
+            hb_context = connection_context.HappyBaseConnectionContext(
                 local_config
             )
             def all_ok(connection, dummy):
@@ -125,3 +135,110 @@ class TestConnectionContext(unittest.TestCase):
                 a_fake_hbase_connection.close_counter,
                 2
             )
+
+class TestPooledConnectionContext(unittest.TestCase):
+    def test_basic_hbase_usage(self):
+        local_config = DotDict({
+          'hbase_host': 'host',
+          'database_name': 'name',
+          'hbase_port': 9090,
+          'hbase_timeout': 9000,
+          'logger': SilentFakeLogger(),
+        })
+        a_fake_hbase_connection = FakeHB_Connection(local_config)
+        a_fake_hbase_pool = FakeHBPoolConnection()
+        with mock.patch.object(
+            happybase,
+            'ConnectionPool',
+            mock.Mock(return_value=a_fake_hbase_connection)
+        ):
+            hb_context = connection_context.HappyBasePooledConnectionContext(
+                local_config
+            )
+            # open a connection
+            with hb_context() as conn:
+                pass
+            self.assertEqual(
+                a_fake_hbase_connection.close_counter,
+                1
+            )
+            # open another connection again
+            with hb_context() as conn:
+                pass
+            self.assertEqual(
+                a_fake_hbase_connection.close_counter,
+                2
+            )
+            # get a named connection
+            with hb_context('fred') as conn:
+                pass
+            self.assertEqual(
+                a_fake_hbase_connection.close_counter,
+                3
+            )
+            # close all connections
+            hb_context.close()
+            self.assertEqual(
+                a_fake_hbase_connection.close_counter,
+                3
+            )
+
+    def test_hbase_usage_with_transaction(self):
+        local_config = DotDict({
+          'hbase_host': 'host',
+          'database_name': 'name',
+          'hbase_port': 9090,
+          'hbase_timeout': 9000,
+          'number_of_retries': 2,
+          'logger': SilentFakeLogger(),
+        })
+        a_fake_hbase_connection = FakeHB_Connection(local_config)
+        with mock.patch.object(
+            HappyBasePooledConnectionContext,
+            'connection',
+            mock.Mock(return_value=a_fake_hbase_connection)
+        ):
+            hb_context = connection_context.HappyBasePooledConnectionContext(
+                local_config
+            )
+            def all_ok(connection, dummy):
+                self.assertEqual(dummy, 'hello')
+                return True
+
+            transaction = TransactionExecutor(local_config, hb_context)
+            result = transaction(all_ok, 'hello')
+            self.assertTrue(result)
+            self.assertEqual(
+                a_fake_hbase_connection.close_counter,
+                1
+            )
+            self.assertEqual(
+                a_fake_hbase_connection.rollback_counter,
+                0
+            )
+            self.assertEqual(
+                a_fake_hbase_connection.commit_counter,
+                1
+            )
+
+            def bad_deal(connection, dummy):
+                raise KeyError('fred')
+
+            self.assertRaises(KeyError, transaction, bad_deal, 'hello')
+            self.assertEqual(
+                a_fake_hbase_connection.close_counter,
+                2
+            )
+            self.assertEqual(
+                a_fake_hbase_connection.commit_counter,
+                1
+            )
+
+            hb_context.close()
+            self.assertEqual(
+                a_fake_hbase_connection.close_counter,
+                2
+            )
+
+
+
